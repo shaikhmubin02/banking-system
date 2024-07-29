d4b231b2ff8c9f7c9fdd12e33b3355d43270edef
shaikhmubin02 <shaikhmubin572@gmail.com>
2024-07-30 00:25:23 +0530

    init

--- app/(route)/layout.tsx
+++ app/(route)/layout.tsx
@@ -1,11 +1,17 @@
+import Sidebar from "@/components/Sidebar";
+
+
 export default function RootLayout({
     children,
   }: Readonly<{
     children: React.ReactNode;
-  }>) {
+  }>) { 
+    const loggedIn = {firstName: 'Mubin', lastName: 'Shaikh'};
+
     return (
-      <main> 
-        SIDEBAR 
+      <main className="flex h-screen w-full font-inter">  
+        <Sidebar user={loggedIn} />   
+        
         {children}
       </main>
     );
